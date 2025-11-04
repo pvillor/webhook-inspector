75279190d8014853510971c06406b53dc570693a
Paulo Guerra <paulo.guerra@kodigos.com.br>
2025-11-04 12:42:20 -0400

    feat: web structure

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './app'
+import ReactDOM from 'react-dom/client'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css'
+import { routeTree } from './routeTree.gen'
+import { StrictMode } from 'react'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
---
+++ web/src/routes/__root.tsx
@@ -0,0 +1,5 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+
+const RootLayout = () => <Outlet />
+
+export const Route = createRootRoute({ component: RootLayout })
